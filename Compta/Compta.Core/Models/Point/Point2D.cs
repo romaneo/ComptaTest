44466d047b48d8c8e70ac0f850af295f638259ab
Romaneo <romaneo95@gmail.com>
2015-09-23 18:49:29 +0300

    Add class for 2D point

---
+++ Compta/Compta.Core/Models/Point/Point2D.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Compta.Core.Models.Point
+{
+    class Point2D<T> : IPoint
+    {
+        #region X property
+        private T _x;
+
+        /// <summary>
+        /// Gets the x-coordinate of this 2D point
+        /// </summary>
+        public T X
+        {
+            get
+            {
+                return _x;
+            }
+        }
+        #endregion
+
+        #region Y property
+
+        private T _y;
+
+        /// <summary>
+        /// Gets the y-coordinate of this 2D point
+        /// </summary>
+        public T Y
+        {
+            get
+            {
+                return _y;
+            }
+        }
+
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Initialize a new instance of the 2D point with the specified coordinates
+        /// </summary>
+        /// <param name="x">The horizontal position of the point </param>
+        /// <param name="y">The vertical position of the point </param>
+        public Point2D(T x, T y)
+        {
+            _x = x;
+            _y = y;
+        }
+
+        /// <summary>
+        /// Initialize a new instance of the 2D point with default value
+        /// </summary>
+        public Point2D()
+        {
+            _x = default(T);
+        } 
+        #endregion
+    }
+}
