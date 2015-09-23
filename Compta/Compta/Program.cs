66f2cc04e128dabb32ab8366023b16c2530c9eda
Romaneo <romaneo95@gmail.com>
2015-09-23 18:31:58 +0300

    Initial commit

---
+++ Compta/Compta/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Compta.Core.Models;
+using Compta.Core.Models.Point;
+
+
+namespace Compta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("\n\ncompleted!".ToUpper());
+            Console.ReadKey();
+        }
+    }
+}
