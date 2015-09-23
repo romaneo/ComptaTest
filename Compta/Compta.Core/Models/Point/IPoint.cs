7e116e2de3ae7947708686adf52468ac2425f7eb
Romaneo <romaneo95@gmail.com>
2015-09-23 18:34:30 +0300

    Add class for 1D point

---
+++ Compta/Compta.Core/Models/Point/IPoint.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Compta.Core.Models.Point
+{
+    /// <summary>
+    /// Represent a base data for different points values
+    /// </summary>
+    public interface IPoint
+    {
+
+    }
+}
---
+++ Compta/Compta.Core/Models/Point/Point1D.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Compta.Core.Models.Point
+{
+    public class Point1D<T> : IPoint
+    {
+
+        private T _x;
+
+        /// <summary>
+        /// Represent single numerical value
+        /// </summary>
+        public T X
+        {
+            get
+            {
+                return _x;
+            }
+        }
+        /// <summary>
+        /// Initialize a new instance of the 1D point
+        /// </summary>
+        /// <param name="x">The position of the point </param>
+        public Point1D(T x)
+        {
+            _x = x;
+        }
+
+        /// <summary>
+        /// Initialize a new instance of the 1D point with default value
+        /// </summary>
+        public Point1D()
+        {
+            _x = default(T);
+        }
+    }
+}
