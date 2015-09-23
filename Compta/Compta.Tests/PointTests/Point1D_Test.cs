9282d272f137def62da37951a06f618e9ca40b69
Romaneo <romaneo95@gmail.com>
2015-09-23 19:29:08 +0300

    Tests for Point1D

---
+++ Compta/Compta.Tests/PointTests/Point1D_Test.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Compta.Core.Models.Point;
+
+namespace Compta.Tests
+{
+    [TestClass]
+    public class Point1D_Test
+    {
+        [TestMethod]
+        public void Create1DPoint_WithDefaultValue()
+        {
+            var point = new Point1D<int>();
+            Assert.IsNotNull(point);
+            Assert.AreEqual(point.X, 0);
+        }
+
+        [TestMethod]
+        public void Create1DPoint_WithUserValue()
+        {
+            var point = new Point1D<int>(97);
+            Assert.AreEqual(point.X, 97);
+        }
+    }
+}
